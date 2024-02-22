33f4e6c6d0d508a8ab9d68f1fb71ede493fdd8aa
dannyzshaver <dshaver@u.northwestern.edu>
2024-02-21 21:16:59 -0600

    fixed_branch_unit

--- colincode/pl_util_lib.v
+++ colincode/pl_util_lib.v
@@ -162,24 +162,24 @@ module BranchUnit(opA, opB, funct3, opcode, out, halt);
 
   always @(*) begin
     halt = 1'b0;
-    if (opcode == `OPCODE_BRANCH || opcode == `OPCODE_JAL || opcode == `OPCODE_JALR)
-        begin
-            case(funct3)
-            3'b000: out = (opA == opB) ? `BR_TRUE : `BR_FALSE; // beq
-            3'b001: out = (opA != opB) ? `BR_TRUE : `BR_FALSE; // bne
-            3'b100: out = (sopA < sopB) ? `BR_TRUE : `BR_FALSE; // blt
-            3'b101: out = (sopA >= sopB) ? `BR_TRUE : `BR_FALSE; // bge
-            3'b110: out = (opA < opB) ? `BR_TRUE : `BR_FALSE; // bltu
-            3'b111: out = (opA >= opB) ? `BR_TRUE : `BR_FALSE; // bgeu
-            default: 
-                if (opcode == `OPCODE_BRANCH)
-                halt = 1'b1; // if branch opcode but not a valid br instruction, halt
-                else if (opcode == `OPCODE_JAL || opcode == `OPCODE_JALR)
-                out = `BR_TRUE; // if jal or jalr, always branch (jump
-                else
-                out = `BR_FALSE; // default
-            endcase
-        end
+    if (opcode == `OPCODE_JAL || opcode == `OPCODE_JALR) 
+        out = `BR_TRUE; // if jal or jalr, always branch (jump)
+        else if (opcode == `OPCODE_BRANCH)
+            begin
+                case(funct3)
+                3'b000: out = (opA == opB) ? `BR_TRUE : `BR_FALSE; // beq
+                3'b001: out = (opA != opB) ? `BR_TRUE : `BR_FALSE; // bne
+                3'b100: out = (sopA < sopB) ? `BR_TRUE : `BR_FALSE; // blt
+                3'b101: out = (sopA >= sopB) ? `BR_TRUE : `BR_FALSE; // bge
+                3'b110: out = (opA < opB) ? `BR_TRUE : `BR_FALSE; // bltu
+                3'b111: out = (opA >= opB) ? `BR_TRUE : `BR_FALSE; // bgeu
+                default: 
+                    if (opcode == `OPCODE_BRANCH)
+                    halt = 1'b1; // if branch opcode but not a valid br instruction, halt
+                    else
+                    out = `BR_FALSE; // default
+                endcase
+            end
     else 
         out = `BR_FALSE; // default
   end
